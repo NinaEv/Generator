562e2dac7a9e6d83b8dcd9229f48f7c4309f742e
Эверстова Нина <ninaever@mail.ru>
2022-06-04 17:05:31 +0900

    Initial

---
+++ Main.java
@@ -0,0 +1,14 @@
+package jadv.task_6_1;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.Function;
+
+public class Main {
+
+    public static void main(String[] args) {
+        String text = "дорообо аатын ким диэний";
+        Function<String, List<String>> function = (a) -> Arrays.stream(a.split(" ")).sorted().distinct().toList();
+        function.apply(text).forEach(System.out::println);
+    }
+}
